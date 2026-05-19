e8a9322a840332847df63a41c975f5857ef02c32
Sonu Kumar Suman <Sonukumarsuman82@gmail.com>
2026-05-20 00:10:43 +0530

    First set up

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import find_packages,setup
+from typing import List
+
+
+def get_requirements(file_path:str)->List[str]:
+    '''
+    Read function will return the list of requirements
+    '''
+    requirements=[]
+    with open(file_path) as file_obj:
+        requirements=file_obj.readlines()
+        requirements=[req.replace('\n',"") for req in requirements]
+        return requirements
+
+setup(
+    name='ML-Project',
+    version='0.1.0',
+    author='Sonu Kumar Suman',
+    author_email='sonukumarsuman82@gmail.com',
+    packages=find_packages(),
+    install_requires=['numpy', 'pandas']
+)
